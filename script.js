a22ffd9ea4d38f3aa555937bb0bc7f06e9305b48
prashant280920 <jayantmehta8447442229@gmail.com>
2020-05-25 14:01:12 +0530

    modified file

--- script.js
+++ script.js
@@ -2,232 +2,65 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 
-let filename='';
+
 
 const downloadBtn = document.getElementById("download-btn");
 const uploadFile = document.getElementById("upload-file");
 const revertBtn = document.getElementById("revert-btn");
 
 
-/*---------------------- To do filter -------------------*/
 
-var brightness = 0, contrast = 0, saturation = 0, vibrance = 0; //Initialasize the value
-const br = document.getElementById("brightness");
-const co = document.getElementById("contrast");
-const sa = document.getElementById("saturation");
-const vi = document.getElementById("vibrance");
+/*-----------------------------------Add Effects--------------------------------*/
+var dictionary = {"xpro2-add":"contrast(1.3) brightness(0.8) sepia(0.3) saturate(1.5) hue-rotate(-20deg)",
+					"toaster-add":"sepia(0.4) saturate(2.5) hue-rotate(-30deg) contrast(0.67)",
+					"sutro-add":"brightness(0.75) contrast(1.3) sepia(0.5) hue-rotate(-25deg)",
+					"mayfair-add":"saturate(1.4) contrast(1.1)",
+					"kelvin-add":"sepia(0.4) saturate(2.4) brightness(1.3) contrast(1)",
+					'inkwell-add':"grayscale(1) brightness(1.2) contrast(1.05)",
+					"brannan-add":'sepia(0.5) contrast(1.4)',
+					"amaro-add":" hue-rotate(-10deg) contrast(0.9) brightness(1.1) saturate(1.5)",
+					"none":"none"} 
+	// dictionary contain proprty : class of the  button , value : css effects add on canvas 
 
-//Changing filters on clicking buttons
-document.addEventListener("click",(event) => {
-	if(event.target.classList.contains("filter-btn")){
-		//For Brightness
-		if(event.target.classList.contains("brightness-add")){
-			Caman("#canvas",img,function(){
-				this.brightness(+5).render();
-				brightness+=5;
-				br.value = brightness;
-			});
-
-		}
-		else if(event.target.classList.contains("brightness-remove")){
-			Caman("#canvas",img,function(){				
-				this.brightness(-5).render();		
-				brightness-=5;
-				br.value = brightness;
-
-			});
-
-		}
-		// For Contrast
-		else if(event.target.classList.contains("contrast-add")){
-			Caman("#canvas",img,function(){
-				
-				this.contrast(+5).render();
-				contrast+=5;
-				co.value = contrast;
-			});
-
-		}
-		else if(event.target.classList.contains("contrast-remove")){
-			Caman("#canvas",img,function(){
-				this.contrast(-5).render();
-				contrast-=5;
-				co.value = contrast;
-			});
-
-		}
-		// For Saturation
-		else if(event.target.classList.contains("saturation-add")){
-			Caman("#canvas",img,function(){
-				this.saturation(+5).render();
-				saturation+=5;
-				sa.value = saturation;
-			});
-
-		}
-		else if(event.target.classList.contains("saturation-remove")){
-			Caman("#canvas",img,function(){
-				this.saturation(-5).render();
-				saturation-=5;
-				sa.value = saturation;
-			});
-
-		}
-		// For Vibrance
-		else if(event.target.classList.contains("vibrance-add")){
-			Caman("#canvas",img,function(){
-				this.vibrance(+5).render();
-				vibrance+=5;
-				vi.value = vibrance;
-			});
-
-		}
-		else if(event.target.classList.contains("vibrance-remove")){
-			Caman("#canvas",img,function(){
-				this.vibrance(-5).render();
-				vibrance-=5
-				vi.value = vibrance;
-
-			});
-
-		}
-		else if(event.target.classList.contains("vintage-add")){
-			Caman("#canvas",img,function(){
-				this.revert();
-				this.vintage().render();
-				brightness = 0; contrast = 0; saturation = 0; vibrance = 0;
-				br.value = brightness;
-				co.value = contrast;
-				sa.value = saturation;
-				vi.value = vibrance;
-			});
-
-			
-
-		}
-		else if(event.target.classList.contains("Lomo-add")){
-			Caman("#canvas",img,function(){
-				this.revert();
-				this.lomo().render();
-				brightness = 0; contrast = 0; saturation = 0; vibrance = 0;
-
-			});
-
-		}
-		else if(event.target.classList.contains("clarity-add")){
-			Caman("#canvas",img,function(){
-				this.revert();
-				this.clarity().render();
-				brightness = 0; contrast = 0; saturation = 0; vibrance = 0;
-				br.value = brightness;
-				co.value = contrast;
-				sa.value = saturation;
-				vi.value = vibrance;
-			});
-
-		}
-		else if(event.target.classList.contains("sincity-add")){
-			Caman("#canvas",img,function(){
-				this.revert();
-				this.sinCity().render();
-				brightness = 0; contrast = 0; saturation = 0; vibrance = 0;
-				br.value = brightness;
-				co.value = contrast;
-				sa.value = saturation;
-				vi.value = vibrance;
-			});
-
-		}
-		else if(event.target.classList.contains("crossprocess-add")){
-			Caman("#canvas",img,function(){
-				this.revert();
-				this.crossProcess().render();
-				brightness = 0; contrast = 0; saturation = 0; vibrance = 0;
-				br.value = brightness;
-				co.value = contrast;
-				sa.value = saturation;
-				vi.value = vibrance;
-			});
-
-		}
-		else if(event.target.classList.contains("pinhole-add")){
-			Caman("#canvas",img,function(){
-				this.revert();
-				this.pinhole().render();
-				brightness = 0; contrast = 0; saturation = 0; vibrance = 0;
-				br.value = brightness;
-				co.value = contrast;
-				sa.value = saturation;
-				vi.value = vibrance;
-			});
-
-		}
-		else if(event.target.classList.contains("nostalgia-add")){
-			Caman("#canvas",img,function(){
-				this.revert();
-				this.nostalgia().render();
-				brightness = 0; contrast = 0; saturation = 0; vibrance = 0;
-				br.value = brightness;
-				co.value = contrast;
-				sa.value = saturation;
-				vi.value = vibrance;
-			});
-
-		}
-		else if(event.target.classList.contains("hermajesty-add")){
-			Caman("#canvas",img,function(){
-				this.revert();
-				this.herMajesty().render();
-				brightness = 0; contrast = 0; saturation = 0; vibrance = 0;
-				br.value = brightness;
-				co.value = contrast;
-				sa.value = saturation;
-				vi.value = vibrance;
-			});
-
-		}
+function add_effects(filterclass){
+	const file = uploadFile.files[0];
+	const reader = new FileReader();
+	
+	if (file){
+		fileName = file.name;
+		reader.readAsDataURL(file);
 	}
-});
-//Changing filters on changing slider
-// br.addEventListener('change',() => {
-// 	Caman("#canvas",img,function(){
-// 		this.revert();
-// 		brightness = parseInt(br.value);
-// 		this.brightness(brightness).render();
+	//Add image to a canvas
+	reader.addEventListener("load",() =>{
+		img = new Image();
+		img.src = reader.result;
 		
-// 	});
-// });
-// co.addEventListener('change',() => {
-// 	Caman("#canvas",img,function(){
-// 		this.revert();
-// 		contrast = parseInt(co.value);
-// 		this.bcontrast(contrast).render();
-// 	});
-// });
-// sa.addEventListener('change',() => {
-// 	Caman("#canvas",img,function(){
-// 		this.revert();
-// 		saturation = parseInt(sa.value);
-// 		this.saturation(saturation).render();
-// 	});
-// });
-// vi.addEventListener('change',() => {
-// 	Caman("#canvas",img,function(){
-// 		this.revert();
-// 		vibrance = parseInt(vi.value);
-// 		this.vibrance(vibrance).render();
-// 	});
-// });
+		
+		img.addEventListener("load" , () => {
+			canvas.width = img.width;
+			canvas.height = img.height;
+			ctx.filter = dictionary[filterclass];
+			ctx.drawImage(img,0,0,img.width,img.height);
+			
+		});
+	},false);
+};
 
-//Revert Filter
-revertBtn.addEventListener("click", e => {
-	Caman("#canvas", img, function(){
-		this.revert();
-	});
+document.addEventListener("click",(event) => {
+
+	if(event.target.classList.contains("filter-btn")){
+		add_effects(event.target.classList[1]);		
+	}
+	else if(event.target.id==="revert-btn"){
+		add_effects("none")
+	}
+
 });
 
 
-//Upload File
+
+
+/*-----------------------------Upload File--------------------------------*/
 uploadFile.addEventListener("change", (event) => {
 	const file = uploadFile.files[0];
 	const reader = new FileReader();
@@ -243,9 +76,48 @@ uploadFile.addEventListener("change", (event) => {
 		img.addEventListener("load" , () => {
 			canvas.width = img.width;
 			canvas.height = img.height;
+
 			ctx.drawImage(img,0,0,img.width,img.height);
 			
 		});
 	},false);
 });
 
+/*------------------------------- Download Event-------------------------------*/
+
+downloadBtn.addEventListener("click", () => {
+  	// Get ext
+	const fileExtension = fileName.slice(-4);
+
+	// Init new filename
+	let newFilename;
+
+	  // Check image type
+	if (fileExtension === ".jpg" || fileExtension === ".png") {
+	    // new filename
+		newFilename = fileName.substring(0, fileName.length - 4) + "-edited.jpg";
+	}
+
+	// Call download
+	download(canvas, newFilename);
+});
+
+// Download
+function download(canvas, filename) {
+	// Init event
+	let e;
+	// Create link
+	const link = document.createElement("a");
+
+	// Set props
+	link.download = filename;
+	  
+	// imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+	link.href = canvas.toDataURL("image/jpeg", 0.8);
+	console.log(canvas,link.href)
+	// New mouse event
+	e = new MouseEvent("click");
+	// Dispatch event
+	link.dispatchEvent(e);
+}
